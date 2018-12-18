b422137760917e1ae99bb09dfa24292910c0db85
Jarome Harmsworth <jarome@Jaromes-MBP-2018.local>
2018-12-18 17:11:28 +1100

    Init for PG website v2

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var browsersync = require("browser-sync").create();
+
+
+var htmlSources = ['**/*.html'];
+
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "dist"
+        },
+        port: 3000
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+// Clean assets
+function clean() {
+    return del(["assets"]);
+}
+
+// CSS task
+function css() {
+    return gulp
+        .src("scss/*")
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(gulp.dest("assets/css"))
+        .pipe(browsersync.stream());
+}
+
+// CSS task
+function copyHtml() {
+    return gulp
+        .src("index.html")
+        .pipe(gulp.dest("assets"))
+}
+
+// Watch files
+function watchFiles() {
+    gulp.watch("scss/*", css);
+    gulp.watch("index.html", copyHtml);
+
+    // Tasks
+    gulp.task("css", css);
+    gulp.task("clean", clean);
+}
+
+
+gulp.task(
+    "build",
+    gulp.series(clean, gulp.parallel(css, copyHtml))
+);
+
+// watch
+gulp.task("watch", gulp.parallel(watchFiles));
